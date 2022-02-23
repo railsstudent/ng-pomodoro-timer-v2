78db8d78ff32ecd4037ed50604921037d444dabc
Connie Leung <cleung334@gmail.com>
2022-02-23 23:23:39 +0800

    feat: timer-buttons module

--- src/app/timer/timer.module.ts
+++ src/app/timer/timer.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common'
 import { TimerShellModule } from './timer-shell'
 import { TimerDisplayModule } from './timer-display'
 import { TimerMessageModule } from './timer-message'
+import { TimerButtonsModule } from './timer-buttons/timer-buttons.module'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, TimerDisplayModule, TimerShellModule, TimerMessageModule],
+  imports: [CommonModule, TimerDisplayModule, TimerShellModule, TimerMessageModule, TimerButtonsModule],
   exports: [TimerShellModule],
 })
 export class TimerModule {}
